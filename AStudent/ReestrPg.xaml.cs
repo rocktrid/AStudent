775db9ce82cf7ac672ab9f73523a84ff1a18689a
Rock Trident <rocktrident19@gmail.com>
2021-04-10 13:26:14 +0300

    Добавлена загрузка .csv файла

--- AStudent/ReestrPg.xaml.cs
+++ AStudent/ReestrPg.xaml.cs
@@ -14,6 +14,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using MySql.Data.MySqlClient;
+using LINQtoCSV;
+using System.Windows.Forms;
+using System.IO;
 
 /// <summary>
 /// Необходимо припилить функцию изменения сервера БД для оперирования над данными. 
@@ -22,10 +25,12 @@ namespace AStudent
 {
     public partial class ReestrPg : Page
     {
-        private string OpenConnection = "Database = db_for_case_solution; Data Source = 127.0.0.1; User Id = root; Password = Password";
+        private readonly string OpenConnection = "Database = db_for_case_solution; Data Source = 127.0.0.1; User Id = root; Password = Password";
         private string NewQuery = "";
         private MySqlDataAdapter adapter;
         private DataTable DTable;
+        List<string[]> ForDGrid;
+        List<string> ForHeaders;
         public ReestrPg()
         {
             InitializeComponent();
@@ -40,7 +45,7 @@ namespace AStudent
             {
                 BaseConn.BuildConnection = new MySqlConnection(ConnectionToBase);
                 BaseConn.BuildConnection.Open();
-                MessageBox.Show("Подключение к БД реализовано");
+                System.Windows.MessageBox.Show("Подключение к БД реализовано");
                 //Остальные StackPanel раскрываем, чтобы не раскрывать элементы внутри них, коих больше
                 For_ActionBtns.Visibility = Visibility.Visible;
                 WorkBenchStack.Visibility = Visibility.Visible;
@@ -48,8 +53,8 @@ namespace AStudent
             }
             catch (Exception ex)
             {
-                MessageBox.Show("" + ex);
-                MessageBox.Show("Ошибка подключения к БД");
+                System.Windows.MessageBox.Show("" + ex);
+                System.Windows.MessageBox.Show("Ошибка подключения к БД");
             }
         }
         private void CmbChooseTable_Selected(object sender, RoutedEventArgs e)
@@ -85,7 +90,7 @@ namespace AStudent
 
                         break;
                     default:
-                        MessageBox.Show("Не выбрана существующая позиция");
+                        System.Windows.MessageBox.Show("Не выбрана существующая позиция");
                         break;
                 }
                 For_ActionBtns.Visibility = Visibility.Visible;
@@ -99,9 +104,9 @@ namespace AStudent
                     using (BaseConn.BuildConnection = new MySqlConnection(OpenConnection))
                     {
                         BaseConn.BuildConnection.Open();
-                        MySqlCommand FillDGrid = new MySqlCommand(NewQuery, BaseConn.BuildConnection);                       
+                        MySqlCommand FillDGrid = new MySqlCommand(NewQuery, BaseConn.BuildConnection);
                         MySqlDataReader DReader = FillDGrid.ExecuteReader();// инициализирует чтение из БД
-                       // если что, выше объявлены поля для adapter, datatable, newquery, openconnection;
+                                                                            // если что, выше объявлены поля для adapter, datatable, newquery, openconnection;
                         adapter = new MySqlDataAdapter(NewQuery, BaseConn.BuildConnection); // адаптер для работы с записью (необязательно для записи здесь, можно снести в метод на клик
                         // но здесь он нужен для уточнения команд на апдейт удаление и добавление
                         DTable = new DataTable(); // для сохранения данных из БД во "временное хранилище"
@@ -113,16 +118,16 @@ namespace AStudent
                         adapter.DeleteCommand = cb.GetDeleteCommand();
                         Workingbench.AutoGenerateColumns = true; // подрубаем автогенерацию колонок, чтобы выводило те наименования, что в БД находятся
                         Workingbench.ItemsSource = DTable.DefaultView; // привязка данных из datatable  к datagrid
-                        MessageBox.Show("Таблица выведена!");                        
+                        System.Windows.MessageBox.Show("Таблица выведена!");
                         BaseConn.BuildConnection.Close();
                     }
                 }
-                else MessageBox.Show("Ошибка формирования запроса: запрос на выборку пуст");
+                else System.Windows.MessageBox.Show("Ошибка формирования запроса: запрос на выборку пуст");
             }
             catch (Exception ex)
             {
-                MessageBox.Show(""+ex);
-                MessageBox.Show("Ошибка инициализации подключения, повторите снова или обратитесь к системному администратору");
+                System.Windows.MessageBox.Show("" + ex);
+                System.Windows.MessageBox.Show("Ошибка инициализации подключения, повторите снова или обратитесь к системному администратору");
             }
         }
         private void Action_4_Click(object sender, RoutedEventArgs e) //тип обновление update для БД
@@ -131,15 +136,105 @@ namespace AStudent
             {
                 BaseConn.BuildConnection.Open();
                 adapter.Update(DTable);
-                MessageBox.Show("Successfull");
+                System.Windows.MessageBox.Show("Successfull");
                 BaseConn.BuildConnection.Close();
             }
             catch (Exception ex)
             {
-                MessageBox.Show("" + ex);
-                MessageBox.Show("Ошибка инициализации записи в БД");
+                System.Windows.MessageBox.Show("" + ex);
+                System.Windows.MessageBox.Show("Ошибка инициализации записи в БД");
                 BaseConn.BuildConnection.Close();
+            }
+        }
+
+        private void BtnImportCSV_Click(object sender, RoutedEventArgs e)
+        {
+            Workingbench.Columns.Clear();
+
+            // диалоговое окно для открытия файла
+            OpenFileDialog OpenFile = new OpenFileDialog
+            {
+                Filter = "CSV|*.csv"
+            };
+            DataTable DT = new DataTable();
+            if (OpenFile.ShowDialog() == DialogResult.OK)
+            {
+                For_ActionBtns.Visibility = Visibility.Visible;
+                WorkBenchStack.Visibility = Visibility.Visible;
+                LowerActionPanel.Visibility = Visibility.Visible;
+                StreamReader sr = new StreamReader(OpenFile.FileName);
+                ForDGrid = System.IO.File.ReadAllLines(OpenFile.FileName).Select(x => x.Split(';')).ToList(); // помещает в List построчно данные из csv по разделителю ';'
+                //List<string[]> ForDGridClear = ForDGrid.ElementAt<string>(ForDGrid.Count).TrimStart(';'); // здесь должно было быть удаление ненужного символа, но что-то не так.
+                ForHeaders = ForDGrid[0].ToList(); // для добавления заголовков
+                foreach (var Header in ForHeaders)
+                {
+                    DT.Columns.Add(Header);
+                }
+                foreach (var x in ForDGrid.Skip(1))
+                {
+                    if (x.SequenceEqual(ForHeaders))   //LINQ проверка на наличие повторяющихся строки в двух созданных листах
+                        continue;     //Пропуск строки с повторяющимися данными
+                    DT.Rows.Add(x);
+                }
+            }
+            // перенос данных в datagrid
+            Workingbench.ItemsSource = DT.DefaultView;
+        }
+
+        private void BtnExportCSV_Click(object sender, RoutedEventArgs e)
+        {
+            string CheckName = "";
+            string savepath = string.Empty;
+            SaveFileDialog SaveFile = new SaveFileDialog
+            {
+                Filter = "CSV (*.csv)|*.csv",
+                FileName = "NewDoc.csv"
+            };
+            if (SaveFile.ShowDialog() == DialogResult.OK)
+            {
+                savepath = System.IO.Path.GetFullPath(SaveFile.FileName);
+                System.Windows.MessageBox.Show("Данные экспортируются в документ");
+                if (File.Exists(CheckName))
+                {
+                    try
+                    {
+                        File.Delete(CheckName);
+                        
+                    }
+                    catch (Exception ex)
+                    {
+                        System.Windows.MessageBox.Show("Ошибка: " + ex.Message);
+                    }
+                }
+                try
+                {
+                    List<string[]> ExpData;
+                    //ExpData = Workingbench.Items;
+                }
+               
+                //int ColumnsCounter = Workingbench.Columns.Count();
+                //string ColumnsNames = "";
+                //DTable.Clear();
+                //DTable = ((DataView)Workingbench.ItemsSource).ToTable();
+                //string[] output = new string[(Convert.ToInt32(DTable.Rows))];
+                //for (int i = 0; i < ColumnsCounter; i++)
+                //{
+                //    ColumnsNames += DTable.Columns[i].ColumnName.ToString() + ";";
+                //}
+                //output[0] += ColumnsNames;
+                //for (int i = 1; (i - 1) < Convert.ToInt32(DTable.Rows); i++)
+                //{
+                //    for (int j = 0; j < ColumnsCounter; j++)
+                //    {
+                //        output[i] += DTable.Rows[i - 1].
+                //    }
+                //}
+                //Add the Header row for CSV file.
+
+                //здесь csv файл 
+
             }
         }
     }
 }
+
